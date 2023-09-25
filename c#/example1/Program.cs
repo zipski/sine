86ed4d65280f6389baf211066407768df9180f3a
Igor Biletskyi <zipski@gmail.com>
2023-09-26 00:14:34 +0300

    4th commit with restored simple user greet

--- c#/example1/Program.cs
+++ c#/example1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hey, dude!");
+Console.WriteLine("Please, enter your name");
+string user = Console.ReadLine();
+Console.Write("Hey, ");
+Console.Write(user)
